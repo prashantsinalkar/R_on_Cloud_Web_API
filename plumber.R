58f7c7719aa8c64ecf84998207756af6b6a4a7c2
prashantsinalkar <prashantsinalkar@gmail.com>
2019-06-29 15:42:49 +0530

    added dynamic file names and formated the code

--- plumber.R
+++ plumber.R
@@ -4,34 +4,41 @@ library(jsonlite)
 library(readr)
 library(futile.logger)
 library(tryCatchLog)
+library(ggplot2)
 
 #* Echo back the input
 #* @param msg The message to echo
 #* @get /echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+function(msg="")
+{
+    list(msg = paste0("The message is: '", msg, "'"))
 }
 
 #* Plot a histogram
 #* @png
 #* @get /plot
-function(){
-  rand <- rnorm(100)
-  hist(rand)
+function()
+{
+    rand <- rnorm(100)
+    hist(rand)
 }
 
 #* @serializer unboxedJSON
 #* @post /rscript
-function(code=""){
-  fileConn<-file("input.R")
-  writeLines(code, fileConn)
-  close(fileConn)
-  ro <- system("Rscript input.R", intern = TRUE)
-  fileConn<-file("output.txt")
-  writeLines(ro, fileConn)
-  close(fileConn)
-  ro <- read_file("output.txt")
-  r<- list(status = "SUCCESS", code = "200", output = ro)
-  return (r)
+function(code="", user_id="")
+{
+    InputFile <- paste("/tmp/",user_id,"/",user_id,".R", sep="")
+    OutputFile <- paste("/tmp/",user_id,"/",user_id,".txt", sep="")
+    RunInputFile <- paste("Rscript", InputFile, sep=" ")
+    fileConn<-file(InputFile)
+    writeLines(code, fileConn)
+    close(fileConn)
+    ro <- system(RunInputFile, intern = TRUE)
+    fileConn<-file(OutputFile)
+    writeLines(ro, fileConn)
+    close(fileConn)
+    ro <- read_file(OutputFile)
+    r<- list(status = "SUCCESS", code = "200", output = ro)
+    return (r)
 }
 
