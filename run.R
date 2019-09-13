8e0265ff6404285a57599978be2bafb52f28e352
prashantsinalkar <prashantsinalkar@gmail.com>
2019-09-13 17:19:40 +0530

    updated run file

--- run.R
+++ run.R
@@ -2,10 +2,14 @@
 library(plumber)
 library(yaml)
 config = yaml.load_file("config.yml")
-r <- plumb("plumber.R")  # Where 'plumber.R' is the location of the file shown above
-r$registerHook("exit", function(){
   print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-  print("Bye bye!")
+  print("Server settings imported.....")
+  print("Server started.....")
+  print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+  r <- plumb("plumber.R")  # Where 'plumber.R' is the location of the file shown above
+  r$registerHook("exit", function(){
+  print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+  print("Server now shutting down .... Bye bye!")
   print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 })
 r$run(host=config$host$host_ip, port=config$host$host_port, swagger=TRUE)
